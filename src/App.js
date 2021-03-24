95a5be68972f559ea1214df0dc399b7517dd89a8
Nilesh <patilnilesh350@gmail.com>
2021-03-23 21:17:28 -0400

    added filter component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 import data from "./data.json";
 
 class App extends React.Component {
@@ -14,6 +15,42 @@ class App extends React.Component {
       sort: "",
     };
   }
+
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
+  }
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    }
+
+    this.setState({
+      size: event.target.value,
+      products: data.products.filter(
+        (product) => product.availableSizes.indexOf(event.target.value) >= 0
+      ),
+    });
+  }; 
+
   render() {
     return (
       <div className="grid-container">
@@ -23,12 +60,19 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
               <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar"> Cart Items</div>
           </div>
         </main>
-        <footer>All rights are reserved</footer>
+        <footer>All rights reserved</footer>
       </div>
     );
   }
